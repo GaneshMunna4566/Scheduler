b63386cf96f05571ca29b50645ca5a4332fb838d
cheera ganesh <cheeraganesh1995@gmail.com>
2023-04-23 20:21:30 -0400

    D
    D

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',csrf_exempt( admin.site.urls)),
     path('', include('jobfiles.urls')),
 ]
